ba6e5b3969c60cebe7abcd832877b56ea2af36dd
Henning Yuen <phenning@foxmail.com>
2020-02-24 14:47:34 +0800

    Update game of life.cpp

--- Game of Life/game of life.cpp
+++ Game of Life/game of life.cpp
@@ -4,35 +4,36 @@
 #include<conio.h>
 #include<windows.h>
 #define CELL "■"
-#define BOARD_SIZE 25
+#define BOARDSIZE 25
 #define DENSITY 0.4
 #define SPEED 3
-const int maxn=BOARD_SIZE+5;
+const int maxn=BOARDSIZE+5;
 int dx[]={-1,-1,-1,0,0,1,1,1},dy[]={-1,0,1,-1,1,-1,0,1};
 int board[maxn][maxn],newboard[maxn][maxn];
-void init_console(HANDLE hout){
+HANDLE hout=GetStdHandle(STD_OUTPUT_HANDLE);
+void init_console(){
   SetConsoleTitle("Game of Life");
-  COORD size={BOARD_SIZE*2,BOARD_SIZE};
+  COORD size={BOARDSIZE*2,BOARDSIZE};
   SetConsoleScreenBufferSize(hout,size);
-  SMALL_RECT rect={0,0,BOARD_SIZE*2-1,BOARD_SIZE-1};
+  SMALL_RECT rect={0,0,BOARDSIZE*2-1,BOARDSIZE-1};
   SetConsoleWindowInfo(hout,1,&rect);
   CONSOLE_CURSOR_INFO cursor_info={1,0};
   SetConsoleCursorInfo(hout,&cursor_info);
 }
-void print_cell(HANDLE hout,int x,int y,bool live){
+void print_cell(int x,int y,bool live){
   COORD pos={(x-1)*2,y-1};
   SetConsoleCursorPosition(hout,pos);
   SetConsoleTextAttribute(hout,live?9:15);
   printf("%s",CELL);
 }
-void new_board(HANDLE hout,int n){
+void init_board(int n){
   for(int i=1;i<=n;i++)
     for(int j=1;j<=n;j++){
       board[i][j]=rand()<=RAND_MAX*DENSITY;
-      print_cell(hout,i,j,board[i][j]);
+      print_cell(i,j,board[i][j]);
   }
 }
-void update_board(HANDLE hout,int n){
+void update_board(int n){
   for(int i=1;i<=n;i++)
     for(int j=1;j<=n;j++){
       int cnt=0;newboard[i][j]=0;
@@ -41,17 +42,16 @@ void update_board(HANDLE hout,int n){
       if(cnt==3)newboard[i][j]=1;
       if(cnt==2)newboard[i][j]=board[i][j];
       if(newboard[i][j]!=board[i][j])
-        print_cell(hout,i,j,newboard[i][j]);
+        print_cell(i,j,newboard[i][j]);
   }
   memcpy(board,newboard,sizeof(board));
 }
 int main(){
   srand(time(0));
-  HANDLE hout=GetStdHandle(STD_OUTPUT_HANDLE);
-  init_console(hout);
-  new_board(hout,BOARD_SIZE);
+  init_console();
+  init_board(BOARDSIZE);
   for(;;){
-    update_board(hout,BOARD_SIZE);
+    update_board(BOARDSIZE);
     Sleep(1000.0/SPEED);
   }
 }
